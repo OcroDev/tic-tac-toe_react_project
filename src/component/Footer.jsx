fb0ee132fe657b428f9021ef2195e84d40b227cf
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2023-04-28 15:36:47 -0400

    adding logo and update footer css

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Footer.css';
+import react  from '../assets/react.svg'
+import javascript from '../assets/javascript_logo.png'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <h4>
-        Ejercicio en React ⚛️
+      <h5>
+         React &nbsp;<img src={react} alt='React logo'/>&nbsp;+&nbsp;<img src={javascript} alt="TypeScript logo" /> &nbsp;
         <span>
           <a href='https://www.github.com/OcroDev'>@OcroDev</a>
         </span>
-      </h4>
-      <h5>Tic-Tac-Toe utilizando solo el hook "useState"</h5>
+      </h5>
+      <h6>&nbsp;Tic-Tac-Toe utilizando solo el hook &nbsp;<b style={{ color: '#07f' }} >"useState()"</b> </h6>
     </footer>
   );
 };
